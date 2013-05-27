ac795ae365ab70b4ab17bbac1b3a01320f4ead7a
frederickxd <frederickeggertsen@hotmail.com>
2013-05-27 10:09:01 +0200

    Changed meta

--- CMS/websted/admin/DEV_BUILD/admin_edit_meta_DEV_BUILD.php
+++ CMS/websted/admin/DEV_BUILD/admin_edit_meta_DEV_BUILD.php
@@ -1,6 +1,6 @@
 <?php require_once('../Connections/cms.php'); ?>
 <?php
-
+// Made by Frederick Eggertsen
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
