b0f8d9a6762de4720a83a9de1f4d0a0d356c8927
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2018-11-12 01:47:49 +0100

    Deprecated: mysql_pconnect()
    http://www.php.net/manual/en/function.mysql-pconnect.php

--- CMS/websted/Connections/cms.php
+++ CMS/websted/Connections/cms.php
@@ -6,5 +6,5 @@ $hostname_cms = "localhost";
 $database_cms = "cms";
 $username_cms = "root";
 $password_cms = "";
-$cms = mysql_pconnect($hostname_cms, $username_cms, $password_cms) or trigger_error(mysql_error(),E_USER_ERROR); 
+$cms = mysqli_connect($hostname_cms, $database_cms, $username_cms, $password_cm) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
