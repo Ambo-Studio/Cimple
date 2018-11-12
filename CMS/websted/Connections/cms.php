00b149ab8a9486de644dd61d3f4284a7eb6cd5ff
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2018-11-12 02:15:32 +0100

    mysqli-select-db-expects-parameter
    Doku

--- CMS/websted/Connections/cms.php
+++ CMS/websted/Connections/cms.php
@@ -7,6 +7,7 @@ $hostname_cms = "localhost";
 $database_cms = "cms";
 $username_cms = "root";
 $password_cms = "";
+# https://stackoverflow.com/questions/13979210/mysqli-select-db-expects-parameter-1-to-be-mysqli-string-given
 $cms = new mysqli($hostname_cms, $username_cms, $password_cms,$database_cms) or (mysqli_connect_error()); 
 
 ?>
