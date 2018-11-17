89b819fd5bbd8cd44935a8a907be36f1e0bb6a7a
Frederick Nicklas Ambo Eggert Eggertsen <frederick@tacaly.com>
2018-11-18 00:12:53 +0100

    Updated folder name

---
+++ unzip_cms/upload/admin_skabelon.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Rediger topmenu</title>
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table width="100%" border="0" cellpadding="0" cellspacing="0" class="layout">
+  <tr>
+    <td colspan="2" class="top"><h1>Administrationsomr&aring;de</h1></td>
+  </tr>
+  <tr>
+    <td class="top1">&nbsp;</td>
+    <td class="top1">&nbsp;</td>
+  </tr>
+  <tr>
+    <td class="left"><div align="center">
+      <p>&nbsp;</p>
+      <p><strong>Rediger topmenu</strong></p>
+      <p>Til admin-forside  </p>
+    </div></td>
+    <td class="middle"><p>&nbsp;</p>
+      <h1>Rediger topmenu </h1></td>
+  </tr>
+</table>
+</body>
+</html>
